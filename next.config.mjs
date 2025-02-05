9319ef2c808907148c25ac7bbe27b479311ae90b
vishalmet <vishalmet003@gmail.com>
2025-02-05 20:44:01 +0530

    redirect routes src dest

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true
-  };
-  
-  export default nextConfig;
+    trailingSlash: true,
+    async redirects() {
+        return [
+            {
+                source: '/wink/:path*',
+                destination: '/',
+                permanent: false,
+            },
+        ];
+    },
+};
+
+export default nextConfig;
