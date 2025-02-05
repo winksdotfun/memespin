8eec4f3820bfc54218ae7c36359d676b913a8fc4
thirumurugan7 <thiru82003@gmail.com>
2025-02-05 22:32:07 +0530

    debug

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+} 
